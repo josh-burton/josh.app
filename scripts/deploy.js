ff1bfafd06900a93103c8dc248f5730d27c61dde
Hugo Nogueira <hugomn@gmail.com>
2018-12-30 20:01:21 +0100

    Add jest config, remove github actions (useless for deploying github pages)

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,18 +4,11 @@ console.log('Starting github pages deploy...');
  
 ghpages.publish('public', {
   branch: 'master',
-  repo: 'https://' + process.env.GITHUB_TOKEN + '@github.com:hugomn/hugomn.github.io.git',
+  repo: 'git@github.com:hugomn/hugomn.github.io.git',
   user: {
     name: 'Hugo Nogueira',
     email: 'hugomn@gmail.com'
   }
-}, function(err) { console.log('Error: ' + err); });
+}, function(err) { console.error('Error: ' + err); });
 
-// ghpages.publish('public', {
-//     branch: 'master',
-//     dest: 'docs',
-//     user: {
-//       name: 'Hugo Nogueira',
-//       email: 'hugomn@gmail.com'
-//     }
-//   }, function(err) { console.log('Error: ' + err); });
+console.log('Deploy finished.');
