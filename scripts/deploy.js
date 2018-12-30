322014162fbbf4cd6ba24c6960a830127b17e917
Hugo Nogueira <hugomn@gmail.com>
2018-12-30 19:43:46 +0100

    Test deploy secret

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,9 +4,18 @@ console.log('Starting github pages deploy...');
  
 ghpages.publish('public', {
   branch: 'master',
-  dest: 'docs',
+  repo: 'git@github.com:hugomn/hugomn.github.io.git',
   user: {
     name: 'Hugo Nogueira',
     email: 'hugomn@gmail.com'
   }
 }, function(err) { console.log('Error: ' + err); });
+
+// ghpages.publish('public', {
+//     branch: 'master',
+//     dest: 'docs',
+//     user: {
+//       name: 'Hugo Nogueira',
+//       email: 'hugomn@gmail.com'
+//     }
+//   }, function(err) { console.log('Error: ' + err); });
